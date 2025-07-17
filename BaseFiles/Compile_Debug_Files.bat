10357fae11ca32d43490be04d651b1e06480a496
sebastian2007bro <70060074+sebastian2007bro@users.noreply.github.com>
2025-07-18 01:29:34 +0200

    Fixs to Base Compiler

--- BaseFiles/Compile_Debug_Files.bat
+++ BaseFiles/Compile_Debug_Files.bat
@@ -7,7 +7,7 @@ mkdir "..\TouchTest\bin\Debug"
 :: Copys "Menu.swfiles" into settings folder
 xcopy /e /Y ".\Base" "..\TouchTest\bin\Debug\"
 
-xcopy /e /Y ".\Base" "..\TouchTest\bin\Debug\"
+::xcopy /e /Y ".\Base" "..\TouchTest\bin\Debug\"
 
 :: This creates the folder that's needed for Compiling TryOS. So DON'T DELETE THE FOLDER!!!
 mkdir "..\TouchTest\bin\Debug\TryOS_Build"
