06a383ce973e77e1a0977bbb754d099e6b0caaaa
sebastian2007bro <70060074+sebastian2007bro@users.noreply.github.com>
2025-11-14 11:09:08 +0100

    Moves half of Form48 to ShellApp
    Moving part of Form48's code into Form48Data. To allow Form48 to work like a ShellApp (Form48 will run like normal without the arg "/UseNewLoader".
    btw "/NewLogin" isn't fully working due to the move of Form48's code)

---
+++ TouchTest/Form48Data.vb
@@ -0,0 +1,26 @@
+﻿Namespace Form48_Things
+    Module Form48Data
+        Public HasBeenOpened As Boolean = False
+
+        Public Sub OpenLogonPage()
+            UI.DisableOpenFramework = True
+            UI.LoadShell("SuperSecretUser", "")
+            Form1.HideTaskbar(True)
+            Form1.OpenChildForm(New Form48)
+            Form1.DisableFullScreenConsole = True
+        End Sub
+
+        Public Sub CloseUSW()
+            Try
+                If Form1.currentForm.Text = "LogonScreen" Then
+                    Form1.currentForm.Close()
+                    Form1.Close()
+                    UI.UserFolder = UI.UsersFolder & "\"
+                End If
+            Catch ex As Exception
+
+            End Try
+
+        End Sub
+    End Module
+End Namespace
